e974648fa700535507a63681010b524cd731a9b1
edscodes <edscodes@gmail.com>
2024-04-14 11:45:18 -0500

    se añade ItemListContainer, se le da estilos y se añade animacion y fuente ggogle fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import NavBar from "./components/NavBar.jsx"
+import ItemListContainer from "./components/ItemListContainer.jsx"
 
 function App() {
   return (
     <>
       <NavBar />
+      <ItemListContainer Titulo={"Las mejores ofertas en tecnologia las encuentras aqui"} />
     </>
   )
 }
